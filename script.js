bee3280ac5f60dfb3875ee103dde41b4b535ecd3
Milos Stefanov <144391186+MilosBC@users.noreply.github.com>
2023-12-09 01:49:43 +0100

    Add new grid functionality
    New grid can now be created on demand with a single click

--- script.js
+++ script.js
@@ -2,24 +2,31 @@
 
 const container = document.querySelector('.container');
 const gridDimensions = document.querySelectorAll('.grid-dimensions');
-let gridElement;
+let gridElements;
+
+let userInput;
 
 const clearGrid = document.querySelector('.clear');
+const newGrid = document.querySelector('.new');
 
 
 function createGrid(num) {
     for (let i = 0; i < num * num; i++) {
-
-           gridElement = document.createElement('div');
+        const gridElement = document.createElement('div');
             gridElement.classList.add('grid-element');
 
             gridElement.style.width = `${900 / num}px`;
+
+            gridElement.addEventListener('mousemove', function() {
+                this.style.backgroundColor = 'black';
+            });
             
             
            
             
             
             container.appendChild(gridElement); 
+           
             
 
         
@@ -29,6 +36,8 @@ function createGrid(num) {
         gridDimensions[i].textContent = `${num}`;
         
     } 
+
+    gridElements = document.querySelectorAll('.grid-element');
 
 
 
@@ -41,9 +50,9 @@ function createGrid(num) {
 createGrid(16);
 
 // storing all the created divs in a NodeList
-const gridElements = document.querySelectorAll('.grid-element');
 
-function makeSketchEffect() {
+
+/*function makeSketchEffect() {
     gridElements.forEach(squareDiv => {
         squareDiv.addEventListener('mousemove', function() {
             squareDiv.style.backgroundColor = 'black';
@@ -52,7 +61,7 @@ function makeSketchEffect() {
 
 }
 
-makeSketchEffect();
+makeSketchEffect(); */
 
 //Clear all sketch effects
 
@@ -60,6 +69,32 @@ clearGrid.addEventListener('click', function() {
     for (let i = 0; i < gridElements.length; i++) {
         gridElements[i].style.backgroundColor = 'white';
     }
+})
+
+// a function that deletes the grid
+
+function deleteGrid() {
+    for (let i = 0; i < gridElements.length; i++) {
+        container.removeChild(gridElements[i]);
+    }
+
+
+}
+
+newGrid.addEventListener('click', function() {
+
+    userInput = Number(prompt(`How many squares per side do you want in the new grid? (maximum allowed number is 100)`));
+
+    if (!userInput || userInput === 0 || userInput > 100) {
+        alert(`Please enter a valid input!`);
+    } else {
+        deleteGrid();
+        createGrid(userInput);
+        //makeSketchEffect();
+        
+    }
+    
+    
 })
 
 
